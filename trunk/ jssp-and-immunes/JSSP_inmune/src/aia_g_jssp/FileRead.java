776fc1ce3f4f94ed3eecff0ce1cf6ddd059fa6fd
epsilon1530@gmail.com <epsilon1530@gmail.com@509cbd4f-34fb-66d0-878a-a1b913a1a2cc>
2012-08-26 00:47:51 +0000

    nuevo leer archivo

--- trunk/ jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/FileRead.java
+++ trunk/ jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/FileRead.java
@@ -4,34 +4,36 @@ import java.util.StringTokenizer;
 
 class FileRead 
 {
- public static void main(String args[])
+public static void main(String args[])
   {
   try{
   // Open the file that is the first 
   // command line parameter
-  FileInputStream fstream = new FileInputStream("C:\\archivos nelson\\proyecto inmune\\JSSP_inmune\\src\\aia_g_jssp\\la01.txt");
+  FileInputStream fstream = new FileInputStream("C:\\archivos nelson\\proyecto inmune\\JSSP_inmune\\src\\aia_g_jssp\\la12.txt");
   // Get the object of DataInputStream
   DataInputStream in = new DataInputStream(fstream);
   BufferedReader br = new BufferedReader(new InputStreamReader(in));
   String strLine;
   //Read File Line By Line
-  Object datos[]=new String[500];
+  Object datos[]=new Object[500];
   
   int c=0;
-
+  
   while ((strLine = br.readLine()) != null)   {
-  // Print the content on the console
-
 	  StringTokenizer str=new  StringTokenizer(strLine);
 	  while(str.hasMoreElements()){
-		  c=c+1;
 		  datos[c]=str.nextElement();
-		  System.out.println (datos[c]);
-		  System.out.println ();
-	  }
-	  datos[c]=Integer.parseInt(strLine);
-	  //System.out.println ("Datos ");
+		  c=c+1;
+		  }
+		  
+  		}
+	  
+  //System.out.println(datos[c]);
   
+  int vector[]=new int [c];
+  for (int i=0;i<c;i++){
+	  vector[i]=Integer.parseInt((String)datos[i] );
+	  System.out.println(vector[i]);
   }
   //Close the input stream
   in.close();
