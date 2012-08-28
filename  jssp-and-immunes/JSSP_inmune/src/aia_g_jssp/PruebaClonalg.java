87644190e98605903237a7c96dba0f8297cb2e1c
epsilon1530 <epsilon1530@gmail.com>
2012-08-28 22:57:25 +0000

    veriRestric

---  jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/PruebaClonalg.java
+++  jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/PruebaClonalg.java
@@ -8,15 +8,14 @@ public class PruebaClonalg{
 				String [] a=algclon.Crear_Poblacion(16.0,tampob);
 				int [] adecode=algclon.decodificar(a);
 				double [] costo=algclon.Funcion_Objetivo(adecode);
+				int[] trabajos={0,1,3,2,2,1,3,3,0,0,1,3,2};
+				algclon.veriRestric(trabajos);
 				for(int i=0;i<adecode.length;i++)
 				{
 					System.out.println(a[i]);
 					System.out.println(adecode[i]);
 					System.out.println(costo[i]);
 				}
-				
-		
-			
 	}
 }
 
