85513d631a548692e039b55e781df13e7a2d2689
epsilon1530@gmail.com <epsilon1530@gmail.com@509cbd4f-34fb-66d0-878a-a1b913a1a2cc>
2012-08-29 23:13:49 +0000

    verificar restricciones.

--- trunk/ jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/PruebaClonalg.java
+++ trunk/ jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/PruebaClonalg.java
@@ -9,12 +9,16 @@ public class PruebaClonalg{
 				int [] adecode=algclon.decodificar(a);
 				double [] costo=algclon.Funcion_Objetivo(adecode);
 				int[] trabajos={0,1,3,2,2,1,3,3,0,0,1,3,2};
-				algclon.veriRestric(trabajos);
+				int[][]instancia={{0,10, 2, 25, 1, 4},{1, 5 ,2 ,25, 0, 8},{0, 23, 1, 13, 2, 7},{2, 8, 0 ,21, 1 ,5}};
+				algclon.veriRestric(trabajos, instancia,4,3);
 				for(int i=0;i<adecode.length;i++)
 				{
-					System.out.println(a[i]);
-					System.out.println(adecode[i]);
-					System.out.println(costo[i]);
+					//int d=i+1;
+					//boolean p=((i+1)%4==0);
+					//System.out.println(p);
+					//System.out.println(a[i]);
+					//System.out.println(adecode[i]);
+					//System.out.println(costo[i]);
 				}
 	}
 }
