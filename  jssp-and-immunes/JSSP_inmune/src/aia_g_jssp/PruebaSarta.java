f0168367ea3df5c763b61a36264e010ff2337c38
epsilon1530 <epsilon1530@gmail.com>
2012-09-25 22:56:25 +0000

    Actualizado PruebaSarta y Sarta.  Desordena y casi codifica en binario

---  jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/PruebaSarta.java
+++  jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/PruebaSarta.java
@@ -1,10 +1,16 @@
 package aia_g_jssp;
 
 public class PruebaSarta {
-    public static void main (String[] args){
-    	Sarta S1=new Sarta();
-    	int resp=S1.Calcular_cant_bits(42);
-    	System.out.println(resp);
-    }
 
+    public static void main (String[] args){
+        Sarta S1=new Sarta();
+        int resp=S1.Calcular_cant_bits(10);
+        int [] Ab=S1.Anticuerpo(10, 5);
+        int [] Abb=S1.Barajar(Ab);
+        //S1.ToBinary(Abb, 10, 5, resp);
+        System.out.println(1/2);
+        System.out.println(0/2);
+        //System.out.println(1%2);
+        //System.out.println(0%2);
+    }
 }
