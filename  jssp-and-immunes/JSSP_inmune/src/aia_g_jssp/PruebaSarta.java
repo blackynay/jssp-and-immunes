5c94fe6b27ed024323db7fd7fc44cd8a27546ee1
epsilon1530 <epsilon1530@gmail.com>
2012-09-26 17:46:44 +0000

    Actualizado Sarta

---  jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/PruebaSarta.java
+++  jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/PruebaSarta.java
@@ -1,15 +1,19 @@
 package aia_g_jssp;
-
 public class PruebaSarta {
 
     public static void main (String[] args){
         Sarta S1=new Sarta();
-        int resp=S1.Calcular_cant_bits(10);
-        int [] Ab=S1.Anticuerpo(10, 5);
+        int J=20;
+        int M=5;
+        int resp=S1.Calcular_cant_bits(J);
+        int [] Ab=S1.Anticuerpo(J, M);
         int [] Abb=S1.Barajar(Ab);
         //S1.ToBinary(Abb, 10, 5, resp);
-        System.out.println(1/2);
-        System.out.println(0/2);
+        S1.ToBinary2(Abb, J, M, resp);
+        //String bin = Integer.toString(Integer.parseInt("9", 10), 2);
+        //System.out.println(bin);
+        //System.out.println(1/2);
+        //System.out.println(0/2);
         //System.out.println(1%2);
         //System.out.println(0%2);
     }
