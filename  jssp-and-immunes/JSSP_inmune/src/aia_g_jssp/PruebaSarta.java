04a58614554307dd03fa59288fab4ccffd6e1fc8
epsilon1530 <epsilon1530@gmail.com>
2012-09-29 01:45:31 +0000

    Genera anticuerpos para cualquier numero de trabajos y maquinas.

---  jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/PruebaSarta.java
+++  jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/PruebaSarta.java
@@ -1,20 +1,20 @@
 package aia_g_jssp;
 public class PruebaSarta {
 
-    public static void main (String[] args){
-        Sarta S1=new Sarta();
-        int J=20;
-        int M=5;
-        int resp=S1.Calcular_cant_bits(J);
-        int [] Ab=S1.Anticuerpo(J, M);
-        int [] Abb=S1.Barajar(Ab);
-        //S1.ToBinary(Abb, 10, 5, resp);
-        S1.ToBinary2(Abb, J, M, resp);
-        //String bin = Integer.toString(Integer.parseInt("9", 10), 2);
-        //System.out.println(bin);
-        //System.out.println(1/2);
-        //System.out.println(0/2);
-        //System.out.println(1%2);
-        //System.out.println(0%2);
+    public static void main(String[] args) {
+        Sarta S1 = new Sarta();
+        int J = 40;
+        int M = 15;
+        int poblacion = 100;
+        String[] pob = new String[poblacion];
+        int resp = S1.Calcular_cant_bits(J);
+   
+        for (int i = 0; i < poblacion; i++) {
+            int[] Ab = S1.Anticuerpo(J, M);
+            int[] Abb = S1.Barajar(Ab);
+            pob[i] = S1.ToBinary2(Abb, J, M, resp);
+            //System.out.println(pob[i]);     
+        }
+        S1.decodificar(pob, J, M,  resp);
     }
 }
