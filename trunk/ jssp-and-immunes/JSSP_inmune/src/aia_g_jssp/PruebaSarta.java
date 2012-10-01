2a2b13ff6763478b80f26386a15e9fda33ff3abb
epsilon1530@gmail.com <epsilon1530@gmail.com@509cbd4f-34fb-66d0-878a-a1b913a1a2cc>
2012-10-01 23:14:20 +0000

    Calcula makespam para la población.

--- trunk/ jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/PruebaSarta.java
+++ trunk/ jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/PruebaSarta.java
@@ -1,20 +1,55 @@
 package aia_g_jssp;
+
 public class PruebaSarta {
 
-    public static void main(String[] args) {
-        Sarta S1 = new Sarta();
-        int J = 40;
-        int M = 15;
-        int poblacion = 100;
-        String[] pob = new String[poblacion];
-        int resp = S1.Calcular_cant_bits(J);
-   
-        for (int i = 0; i < poblacion; i++) {
-            int[] Ab = S1.Anticuerpo(J, M);
-            int[] Abb = S1.Barajar(Ab);
-            pob[i] = S1.ToBinary2(Abb, J, M, resp);
-            //System.out.println(pob[i]);     
-        }
-        S1.decodificar(pob, J, M,  resp);
-    }
+	public static void main(String[] args) {
+		Sarta S1 = new Sarta();
+		int poblacion = 100;
+		String[] pob = new String[poblacion];
+
+		
+		Clonalg algclon=new Clonalg();
+		
+        Leer_archivo inst=new Leer_archivo();
+        int [][]instancia=inst.Leer_instancia();
+        int []Tama=inst.Tamano_inst();
+        
+		int resp = S1.Calcular_cant_bits(Tama[0]);
+		
+		for (int i = 0; i < poblacion; i++) {
+			int[] Ab = S1.Anticuerpo(Tama[0], Tama[1]);			// Anticuerpo sin barajar.
+			int[] Abb = S1.Barajar(Ab);				// Anticuerpo barajado.
+			pob[i] = S1.ToBinary2(Abb, Tama[0], Tama[1], resp);	// pob[i] representa cada individuo en String
+			int[] Anticuerpo = S1.decodificar(pob[i], Tama[0], Tama[1], resp);// Vector entero de anticuerpo
+			int [][][] calendario=algclon.veriRestric(Anticuerpo, instancia,Tama[0],Tama[1]);
+			int Make_span=algclon.Makespan(calendario,Tama[0],Tama[1]);
+			// System.out.println(pob[i]);
+		}
+
+		/*
+		 * // Anticuerpo sin barajar.
+		 * 
+		 * int[] Ab = S1.Anticuerpo(J, M);
+		 * 
+		 * // Anticuerpo barajado.
+		 * 
+		 * int[] Abb = S1.Barajar(Ab);
+		 * 
+		 * // pob[i] representa cada individuo
+		 * 
+		 * pob[0] = S1.ToBinary2(Abb, J, M, resp);
+		 * 
+		 * // System.out.println(pob[i]);
+		 * 
+		 * System.out.print(pob[0]);
+		 * 
+		 * System.out.println();
+		 * 
+		 * for (int i = 0; i < J * M; i++) {
+		 * 
+		 * System.out.print(Abb[i]);
+		 * 
+		 * }System.out.println();
+		 */
+	}
 }
