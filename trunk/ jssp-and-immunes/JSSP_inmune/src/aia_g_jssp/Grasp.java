c73e00a00142dfa61d35eba56cdf861f5ee48042
epsilon1530@gmail.com <epsilon1530@gmail.com@509cbd4f-34fb-66d0-878a-a1b913a1a2cc>
2012-10-13 00:25:03 +0000

    Grasp, con Makespam actualizado.

--- trunk/ jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/Grasp.java
+++ trunk/ jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/Grasp.java
@@ -1,5 +1,38 @@
 package aia_g_jssp;
 
-public class Grasp {
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.List;
 
+public class Grasp extends Sarta{
+
+    public void construct_randomized_greedy_solution(int J,int M){
+        int [] allJobs=new int[J*M];
+        int con = 0;
+        int c = 0;
+        int [][] opciones= new int[J][M];
+        for(int i=0;i<J*M;i++){
+            allJobs[i]=i;
+            opciones[c][con]=c;
+            System.out.print(opciones[c][con]);
+               con++;
+            if(con==M){
+            	c++;
+            	con=0;
+                System.out.println();
+            }
+        }
+        Random randomGenerator = new Random();
+
+        List <Integer> candidato =new  ArrayList<Integer> ();
+        List <Integer> candidates =new  ArrayList<Integer> ();
+       
+        candidato.add(randomGenerator.nextInt(J*M));
+        while(candidato.size() < allJobs.length){
+            for(int i=0;i<allJobs.length;i++){
+
+            }
+        }
+           
+    }
 }
--- trunk/ jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/PruebaGrasp.java
+++ trunk/ jssp-and-immunes/JSSP_inmune/src/aia_g_jssp/PruebaGrasp.java
@@ -1,8 +1,24 @@
 package aia_g_jssp;
 
-public class PruebaGrasp {
-	int max_iter = 200;
-	int max_no_improv = 50;
-	double greediness_factor = 0.3;
 
+public class PruebaGrasp {
+     public static void main(String[] args) {
+        // Parametros algoritmo clonalg
+         double alpha = 0.3;
+          int max_iter = 50;
+          int max_no_improv = 50;
+         
+         // Lectura de la instancia
+         Leer_archivo inst = new Leer_archivo();
+         int[][] instancia = inst.Leer_instancia();
+         int[] Tama = inst.Tamano_inst();
+         int lon_individuo = Tama[0] * Tama[1];
+        
+         Grasp grasp =new Grasp();
+         int allJobs [] = grasp.Anticuerpo(Tama[0], Tama[1]);
+         grasp.construct_randomized_greedy_solution(Tama[0],Tama[1]);
+        
+         //
+     }
 }
+
